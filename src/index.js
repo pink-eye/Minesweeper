dd84abdeeafb20c970bcd914c5c9b2b76f388e93
pink-eye <github@virginson.anonaddy.com>
2021-07-04 15:42:14 +0630

    v1.0.0

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) app.quit();
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        icon: __dirname + '/icon.ico',
+        fullscreen: true,
+        frame: false,
+		webPreferences: {
+			devTools: false,
+		}
+    });
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
